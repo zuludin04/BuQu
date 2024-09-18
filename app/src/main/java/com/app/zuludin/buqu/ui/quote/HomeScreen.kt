f5474f2749e22839bbbb95cfefc20dd66253a830
zuludin <zmauludin04@gmail.com>
2024-09-18 21:52:57 +0700

    fixing home quote ui

--- app/src/main/java/com/app/zuludin/buqu/ui/quote/HomeScreen.kt
+++ app/src/main/java/com/app/zuludin/buqu/ui/quote/HomeScreen.kt
@@ -12,16 +12,19 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
@@ -48,6 +51,7 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -55,6 +59,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.app.zuludin.buqu.domain.models.Quote
 import com.app.zuludin.buqu.ui.addquote.AddQuoteScreen
 import com.app.zuludin.buqu.util.LoadingContent
+import kotlin.random.Random
 
 @Composable
 fun HomeScreen(
@@ -75,9 +80,7 @@ fun HomeScreen(
                 quotes = uiState.quotes,
                 onQuoteClick = { })
             FabContainer(
-                modifier = Modifier
-                    .align(Alignment.BottomEnd),
-                onClick = { /*viewModel.insertQuote("Hallo", "Keigo")*/ }
+                modifier = Modifier.align(Alignment.BottomEnd)
             )
         }
 
@@ -91,7 +94,7 @@ fun HomeScreen(
 }
 
 @Composable
-private fun FabContainer(modifier: Modifier = Modifier, onClick: () -> Unit) {
+private fun FabContainer(modifier: Modifier = Modifier) {
     var containerState by remember {
         mutableStateOf(ContainerState.Fab)
     }
@@ -185,29 +188,27 @@ private fun HotContent(
     onQuoteClick: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-//    Column {
-//        SearchBar(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(horizontal = 12.dp, vertical = 6.dp)
-//        )
-//        HotTakes()
-//    }
-    LoadingContent(loading = loading, empty = quotes.isEmpty() && !loading, emptyContent = {}) {
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(horizontal = 8.dp)
-        ) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 12.dp),
-                verticalArrangement = Arrangement.spacedBy(12.dp)
+    Column {
+        SearchBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 6.dp)
+        )
+        LoadingContent(loading = loading, empty = quotes.isEmpty() && !loading, emptyContent = {}) {
+            Column(
+                modifier = modifier.fillMaxSize()
             ) {
-                items(quotes) {
-                    HotTake(hotTake = it) {
-                        onQuoteClick(it.quoteId)
+                LazyVerticalStaggeredGrid(
+                    columns = StaggeredGridCells.Fixed(2),
+                    modifier = Modifier.fillMaxWidth(),
+                    contentPadding = PaddingValues(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalItemSpacing = 16.dp
+                ) {
+                    items(quotes) {
+                        HotTake(hotTake = it) {
+                            onQuoteClick(it.quoteId)
+                        }
                     }
                 }
             }
@@ -250,25 +251,33 @@ private fun SearchBar(modifier: Modifier = Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HotTake(hotTake: Quote, onClick: () -> Unit) {
+    val color = Color(Random.nextLong(0xffffffff)).copy(alpha = 0.1f)
     Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
+        modifier = Modifier.fillMaxWidth(),
         onClick = onClick,
     ) {
         Column(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .background(color)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = hotTake.author,
+                text = hotTake.book,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.titleMedium,
             )
             Text(
-                modifier = Modifier
-                    .padding(top = 6.dp),
-                maxLines = 3,
+                modifier = Modifier.padding(top = 4.dp),
                 text = hotTake.quote,
-                overflow = TextOverflow.Ellipsis
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = hotTake.author,
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.padding(top = 8.dp)
             )
         }
     }
