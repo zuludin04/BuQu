e4966820707324609bff24b37d3cb8f8d0960193
zuludin <zmauludin04@gmail.com>
2024-09-24 22:28:40 +0700

    fixing add quote ui state

--- app/src/main/java/com/app/zuludin/buqu/ui/addquote/AddQuoteScreen.kt
+++ app/src/main/java/com/app/zuludin/buqu/ui/addquote/AddQuoteScreen.kt
@@ -1,90 +1,122 @@
 package com.app.zuludin.buqu.ui.addquote
 
-import android.widget.Toast
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.core.Animatable
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import kotlin.math.roundToInt
+import com.app.zuludin.buqu.R
 
 @Composable
 fun AddQuoteScreen(
     modifier: Modifier = Modifier,
     onBack: () -> Unit,
-    viewModel: AddQuoteViewModel = hiltViewModel()
+    viewModel: AddQuoteViewModel = hiltViewModel(),
+    scaffoldState: ScaffoldState = rememberScaffoldState()
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     BackHandler {
         onBack()
+        scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+        viewModel.onPopScreen()
     }
-    Scaffold { contentPadding ->
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            Toolbar(onBack = {
+                onBack()
+                scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+                viewModel.onPopScreen()
+            })
+        },
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_mic),
+                            contentDescription = "Localized description"
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_camera),
+                            contentDescription = "Localized description",
+                        )
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = viewModel::saveQuote,
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_check),
+                            "Localized description"
+                        )
+                    }
+                }
+            )
+        }
+    ) { contentPadding ->
         Column(
             modifier = modifier
                 .padding(contentPadding)
+                .padding(horizontal = 16.dp)
         ) {
-            Toolbar(onBack = onBack, onSave = viewModel::saveQuote)
+            val focusManager = LocalFocusManager.current
+
             TitleInputField(
                 modifier = Modifier.fillMaxWidth(),
                 singleLine = false,
                 label = "Quote",
                 capitalization = KeyboardCapitalization.Sentences,
                 value = uiState.quote,
-                onChanged = viewModel::updateQuote
+                onChanged = viewModel::updateQuote,
+                imeAction = ImeAction.Next,
+                keyboardAction = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    }
+                )
             )
             Row {
                 TitleInputField(
@@ -92,14 +124,27 @@ fun AddQuoteScreen(
                     label = "Book",
                     capitalization = KeyboardCapitalization.Words,
                     value = uiState.book,
-                    onChanged = viewModel::updateBook
+                    onChanged = viewModel::updateBook,
+                    imeAction = ImeAction.Next,
+                    keyboardAction = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Right)
+                        }
+                    )
                 )
+                Box(modifier = Modifier.padding(horizontal = 8.dp))
                 TitleInputField(
                     modifier = Modifier.weight(1f),
                     label = "Page",
                     keyboardType = KeyboardType.Number,
-                    value = uiState.page.toString(),
-                    onChanged = viewModel::updatePage
+                    value = uiState.page,
+                    onChanged = viewModel::updatePage,
+                    imeAction = ImeAction.Next,
+                    keyboardAction = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Down)
+                        }
+                    )
                 )
             }
             TitleInputField(
@@ -107,31 +152,38 @@ fun AddQuoteScreen(
                 label = "Author",
                 capitalization = KeyboardCapitalization.Words,
                 value = uiState.author,
-                onChanged = viewModel::updateAuthor
+                onChanged = viewModel::updateAuthor,
+                imeAction = ImeAction.Done,
+                keyboardAction = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                    }
+                )
             )
         }
     }
 
-    LaunchedEffect(uiState.isQuoteSaved) {
+    LaunchedEffect(uiState) {
         if (uiState.isQuoteSaved) {
             onBack()
+            scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+            viewModel.onPopScreen()
+        }
+
+        if (uiState.isError) {
+            scaffoldState.snackbarHostState.showSnackbar("Make sure to fill all forms")
+            viewModel.errorMessageShown()
         }
     }
 }
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-private fun Toolbar(onBack: () -> Unit, onSave: () -> Unit) {
-    CenterAlignedTopAppBar(
+private fun Toolbar(onBack: () -> Unit) {
+    TopAppBar(
         windowInsets = WindowInsets(0, 0, 0, 0),
-        title = {},
-        actions = {
-            FilledTonalButton(
-                modifier = Modifier.padding(end = 6.dp),
-                onClick = onSave,
-            ) {
-                Text(text = "Save")
-            }
+        title = {
+            Text("Add Quote")
         },
         navigationIcon = {
             FilledTonalIconButton(
@@ -154,13 +206,15 @@ private fun TitleInputField(
     label: String,
     modifier: Modifier = Modifier,
     singleLine: Boolean = true,
+    value: String,
+    onChanged: (String) -> Unit,
     capitalization: KeyboardCapitalization = KeyboardCapitalization.None,
     keyboardType: KeyboardType = KeyboardType.Text,
-    value: String,
-    onChanged: (String) -> Unit
+    imeAction: ImeAction = ImeAction.Default,
+    keyboardAction: KeyboardActions = KeyboardActions.Default
 ) {
     OutlinedTextField(
-        modifier = modifier,
+        modifier = modifier.padding(vertical = 4.dp),
         value = value,
         singleLine = singleLine,
         label = {
@@ -169,7 +223,11 @@ private fun TitleInputField(
                 color = Color.Gray,
             )
         },
-        onValueChange = onChanged,
+        onValueChange = { v ->
+            if (v.isNotEmpty()) {
+                onChanged(v)
+            }
+        },
         placeholder = {
             Text(
                 text = label,
@@ -179,117 +237,8 @@ private fun TitleInputField(
         keyboardOptions = KeyboardOptions(
             capitalization = capitalization,
             keyboardType = keyboardType,
-        )
+            imeAction = imeAction,
+        ),
+        keyboardActions = keyboardAction,
     )
-}
-
-@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
-@Composable
-private fun SwipeableSample() {
-    val width = 96.dp
-    val squareSize = LocalConfiguration.current.screenWidthDp.dp
-
-    val swipeableState = rememberSwipeableState(1)
-    val sizePx = with(LocalDensity.current) { squareSize.toPx() }
-    val anchors = mapOf(0f to 0, sizePx to 1) // Maps anchor points (in px) to states
-    val context = LocalContext.current
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .swipeable(
-                state = swipeableState,
-                anchors = anchors,
-                thresholds = { _, _ -> FractionalThreshold(0.3f) },
-                orientation = Orientation.Horizontal,
-            )
-            .background(Color.LightGray)
-    ) {
-        Box(
-            Modifier
-                .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
-                .size(48.dp)
-                .background(Color.DarkGray)
-        )
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun SwipeableVoiceNoteComponent() {
-    // Track drag offset state
-    var offsetX by remember { mutableFloatStateOf(0f) }
-    val coroutineScope = rememberCoroutineScope()
-    val context = LocalContext.current
-    var swipeable by remember { mutableStateOf(false) }
-    val haptics = LocalHapticFeedback.current
-
-    // Animate swipe back to initial position
-    val animatedOffsetX = remember { Animatable(0f) }
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(70.dp)
-            .background(Color.DarkGray, shape = CircleShape)
-            .pointerInput(Unit) {
-                detectDragGesturesAfterLongPress(
-                    onDragStart = {
-                        haptics.performHapticFeedback(HapticFeedbackType.LongPress)
-                        swipeable = true
-                        offsetX = 0f
-                    },
-                    onDrag = { change, dragAmount ->
-                        if (swipeable) {
-                            change.consume() // consume the touch event
-                            offsetX += dragAmount.x // update drag offset
-                        }
-                    },
-                    onDragEnd = {
-                        offsetX = 0f
-                        swipeable = false
-                        // Reset the drag offset with animation
-                    }
-                )
-            }
-            .offset { IntOffset(offsetX.toInt(), 0) }
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            // The voice note play button
-            Icon(
-                imageVector = Icons.Default.PlayArrow,
-                contentDescription = "Play",
-                tint = Color.White,
-                modifier = Modifier
-                    .combinedClickable(
-                        onClick = {
-                            Toast
-                                .makeText(context, "Click", Toast.LENGTH_SHORT)
-                                .show()
-                        },
-                        onLongClick = {
-                            swipeable = true
-                            Toast
-                                .makeText(context, "Long Click", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                    )
-                    .size(40.dp)
-            )
-
-            // The drag icon or voice note content
-            Text(
-                text = "Swipe to delete",
-                color = Color.White,
-                modifier = Modifier
-                    .offset { IntOffset(animatedOffsetX.value.toInt(), 0) }
-                    .padding(horizontal = 16.dp)
-            )
-        }
-    }
 }
--- app/src/main/java/com/app/zuludin/buqu/ui/addquote/AddQuoteViewModel.kt
+++ app/src/main/java/com/app/zuludin/buqu/ui/addquote/AddQuoteViewModel.kt
@@ -15,7 +15,8 @@ data class AddQuoteUiState(
     val book: String = "",
     val page: String = "",
     val author: String = "",
-    val isQuoteSaved: Boolean = false
+    val isQuoteSaved: Boolean = false,
+    val isError: Boolean = false,
 )
 
 @HiltViewModel
@@ -26,14 +27,24 @@ class AddQuoteViewModel @Inject constructor(private val upsertQuoteUseCase: Upse
 
     fun saveQuote() = viewModelScope.launch {
         val state = uiState.value
-        upsertQuoteUseCase.invoke(
-            quote = state.quote,
-            book = state.book,
-            author = state.author,
-            page = state.page
-        )
-        _uiState.update {
-            it.copy(isQuoteSaved = true)
+        if (state.quote.isNotEmpty() &&
+            state.book.isNotEmpty() &&
+            state.author.isNotEmpty() &&
+            state.page.isNotEmpty()
+        ) {
+            upsertQuoteUseCase.invoke(
+                quote = state.quote,
+                book = state.book,
+                author = state.author,
+                page = state.page,
+            )
+            _uiState.update {
+                it.copy(isQuoteSaved = true, isError = false)
+            }
+        } else {
+            _uiState.update {
+                it.copy(isError = true)
+            }
         }
     }
 
@@ -58,6 +69,25 @@ class AddQuoteViewModel @Inject constructor(private val upsertQuoteUseCase: Upse
     fun updateAuthor(newAuthor: String) {
         _uiState.update {
             it.copy(author = newAuthor)
+        }
+    }
+
+    fun errorMessageShown() {
+        _uiState.update {
+            it.copy(isError = false)
+        }
+    }
+
+    fun onPopScreen() {
+        _uiState.update {
+            it.copy(
+                quote = "",
+                book = "",
+                page = "",
+                author = "",
+                isQuoteSaved = false,
+                isError = false,
+            )
         }
     }
 }
