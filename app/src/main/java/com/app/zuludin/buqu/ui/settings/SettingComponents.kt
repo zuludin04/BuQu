e6ccf8491efee4086d8cbe6a8eb1bb38646cda26
zuludin <zmauludin04@gmail.com>
2024-12-13 22:53:40 +0700

    feat: setup setting screen

---
+++ app/src/main/java/com/app/zuludin/buqu/ui/settings/SettingComponents.kt
@@ -0,0 +1,146 @@
+package com.app.zuludin.buqu.ui.settings
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingsGroup(
+    name: String,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(modifier = Modifier.padding(vertical = 8.dp)) {
+        Text(name)
+        Spacer(modifier = Modifier.height(8.dp))
+        Surface(
+            color = MaterialTheme.colorScheme.surfaceVariant,
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(4),
+        ) {
+            Column {
+                content()
+            }
+        }
+    }
+}
+
+@Composable
+fun SettingsClickableComp(
+    @DrawableRes icon: Int,
+    iconDesc: String,
+    name: String,
+    onClick: () -> Unit
+) {
+    Surface(
+        color = Color.Transparent,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        onClick = onClick,
+    ) {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painterResource(id = icon),
+                        contentDescription = iconDesc,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            color = MaterialTheme.colorScheme.surfaceTint
+                        ),
+                        modifier = Modifier
+                            .padding(16.dp),
+                        textAlign = TextAlign.Start,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1.0f))
+                Icon(
+                    Icons.Rounded.KeyboardArrowRight,
+                    tint = MaterialTheme.colorScheme.surfaceTint,
+                    contentDescription = null
+                )
+            }
+            Divider(color = MaterialTheme.colorScheme.onSurfaceVariant)
+        }
+    }
+}
+
+@Composable
+fun SettingsSwitchComp(
+    @DrawableRes icon: Int,
+    iconDesc: String,
+    name: String,
+//    state: State<Boolean>,
+    onClick: () -> Unit
+) {
+    Surface(
+        color = Color.Transparent,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        onClick = onClick,
+    ) {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painterResource(id = icon),
+                        contentDescription = iconDesc,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = name,
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            color = MaterialTheme.colorScheme.surfaceTint
+                        ),
+                        textAlign = TextAlign.Start,
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Switch(
+                    checked = false,
+                    onCheckedChange = { onClick() }
+                )
+            }
+            Divider(color = MaterialTheme.colorScheme.onSurfaceVariant)
+        }
+    }
+}
--- app/src/main/java/com/app/zuludin/buqu/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/app/zuludin/buqu/ui/settings/SettingsScreen.kt
@@ -1,17 +1,74 @@
 package com.app.zuludin.buqu.ui.settings
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.app.zuludin.buqu.R
+import com.app.zuludin.buqu.util.BuQuToolbar
+
+/**
+ * General
+ *  Category
+ *  Theme
+ *  Reset
+ * Other
+ *  About
+ *  Rate
+ * */
 
 @Composable
-fun SettingsScreen(modifier: Modifier = Modifier) {
-    Scaffold { paddingValues ->
-        Box(modifier = modifier.padding(paddingValues)) {
-            Text("Settings")
+fun SettingsScreen() {
+    Scaffold(
+        topBar = { BuQuToolbar("Settings") },
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            SettingsGroup(name = "General") {
+                SettingsClickableComp(
+                    name = "Category",
+                    icon = R.drawable.ic_category,
+                    iconDesc = "",
+                    onClick = { },
+                )
+                SettingsSwitchComp(
+                    name = "Theme",
+                    icon = R.drawable.ic_theme,
+                    iconDesc = "",
+//                    state =
+                ) {
+
+                }
+                SettingsClickableComp(
+                    name = "Reset",
+                    icon = R.drawable.ic_reset,
+                    iconDesc = "",
+                    onClick = { },
+                )
+            }
+
+            SettingsGroup(name = "Other") {
+                SettingsClickableComp(
+                    name = "About",
+                    icon = R.drawable.ic_about,
+                    iconDesc = "",
+                    onClick = { },
+                )
+                SettingsClickableComp(
+                    name = "Rate",
+                    icon = R.drawable.ic_rate,
+                    iconDesc = "",
+                    onClick = { },
+                )
+            }
         }
     }
 }
