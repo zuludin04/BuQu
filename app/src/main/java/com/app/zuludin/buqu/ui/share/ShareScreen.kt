013047d5946daede313ee1a13809e7a79bab82ea
zuludin <zmauludin04@gmail.com>
2025-01-01 11:17:23 +0700

    fix: share image as quote

--- app/src/main/java/com/app/zuludin/buqu/ui/share/ShareScreen.kt
+++ app/src/main/java/com/app/zuludin/buqu/ui/share/ShareScreen.kt
@@ -1,5 +1,15 @@
 package com.app.zuludin.buqu.ui.share
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.createChooser
+import android.graphics.Bitmap
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,20 +17,42 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.graphics.layer.drawLayer
+import androidx.compose.ui.graphics.rememberGraphicsLayer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.FileProvider
+import com.app.zuludin.buqu.R
 import com.app.zuludin.buqu.core.compose.BuQuToolbar
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.io.File
+import java.io.FileOutputStream
+import kotlin.coroutines.resume
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun ShareScreen(
     book: String,
@@ -28,6 +60,46 @@ fun ShareScreen(
     author: String,
     onBack: () -> Unit,
 ) {
+    val context = LocalContext.current
+    val graphicsLayer = rememberGraphicsLayer()
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    val writeStorageAccessState = rememberMultiplePermissionsState(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            emptyList()
+        } else {
+            listOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+    )
+
+    fun shareQuote() {
+        if (writeStorageAccessState.allPermissionsGranted) {
+            coroutineScope.launch {
+                val bitmap = graphicsLayer.toImageBitmap()
+//                val uri = bitmap.asAndroidBitmap().saveToDisk(context)
+                context.shareImage(
+                    "Share Image",
+                    bitmap.asAndroidBitmap(),
+                    "screenshot-${System.currentTimeMillis()}.png"
+                )
+            }
+        } else if (writeStorageAccessState.shouldShowRationale) {
+            coroutineScope.launch {
+                val result = snackbarHostState.showSnackbar(
+                    message = "The storage permission is needed to save the image",
+                    actionLabel = "Grant Access"
+                )
+
+                if (result == SnackbarResult.ActionPerformed) {
+                    writeStorageAccessState.launchMultiplePermissionRequest()
+                }
+            }
+        } else {
+            writeStorageAccessState.launchMultiplePermissionRequest()
+        }
+    }
+
     Scaffold(
         topBar = {
             BuQuToolbar(
@@ -38,16 +110,40 @@ fun ShareScreen(
                     }
                 },
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { shareQuote() },
+                content = { Icon(painterResource(R.drawable.ic_share), null) }
+            )
         }
     ) { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
-            QuoteShareContainer(book, quote, author)
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+                    .drawWithCache {
+                        onDrawWithContent {
+                            graphicsLayer.record {
+                                this@onDrawWithContent.drawContent()
+                            }
+                            drawLayer(graphicsLayer)
+                        }
+                    }
+            ) {
+                QuoteShareContainer(book, quote, author)
+            }
         }
     }
 }
 
 @Composable
-private fun QuoteShareContainer(book: String, quote: String, author: String) {
+private fun QuoteShareContainer(
+    book: String,
+    quote: String,
+    author: String
+) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -62,12 +158,88 @@ private fun QuoteShareContainer(book: String, quote: String, author: String) {
     }
 }
 
+private suspend fun Bitmap.saveToDisk(context: Context): Uri {
+    val file = File(
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+        "screenshot-${System.currentTimeMillis()}.png"
+    )
+
+    file.writeBitmap(this, Bitmap.CompressFormat.PNG, 100)
+
+    return scanFilePath(context, file.path) ?: throw Exception("File could not be saved")
+}
+
+private suspend fun scanFilePath(context: Context, filePath: String): Uri? {
+    return suspendCancellableCoroutine { continuation ->
+        MediaScannerConnection.scanFile(
+            context,
+            arrayOf(filePath),
+            arrayOf("image/png")
+        ) { _, scannedUri ->
+            if (scannedUri == null) {
+                continuation.cancel(Exception("File $filePath could not be scanned"))
+            } else {
+                continuation.resume(scannedUri)
+            }
+        }
+    }
+}
+
+fun Context.shareImage(title: String, image: Bitmap, filename: String) {
+    val file = try {
+        val outputFile = File(cacheDir, filename)
+        val outPutStream = FileOutputStream(outputFile)
+        image.compress(Bitmap.CompressFormat.PNG, 100, outPutStream)
+        outPutStream.flush()
+        outPutStream.close()
+        outputFile
+    } catch (e: Throwable) {
+        return toast(e)
+    }
+    val uri = file.toUriCompat(this)
+    val shareIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        type = "image/png"
+        putExtra(Intent.EXTRA_STREAM, uri)
+    }
+    startActivity(Intent.createChooser(shareIntent, title))
+}
+
+fun File.toUriCompat(context: Context): Uri {
+    return FileProvider.getUriForFile(context, context.packageName + ".provider", this)
+}
+
+fun Context.toast(throwable: Throwable) =
+    throwable.message?.let { toast(it) }
+        ?: toast("R.string.unknown_error")
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+private fun File.writeBitmap(bitmap: Bitmap, format: Bitmap.CompressFormat, quality: Int) {
+    outputStream().use { out ->
+        bitmap.compress(format, quality, out)
+        out.flush()
+    }
+}
+
+private fun shareBitmap(context: Context, uri: Uri) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "image/png"
+        putExtra(Intent.EXTRA_STREAM, uri)
+        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    }
+    startActivity(context, createChooser(intent, "Share your image"), null)
+}
+
+
 @Preview
 @Composable
 private fun QuoteShareContainerPreview() {
-    QuoteShareContainer(
-        author = "ASA",
-        quote = "Asa saasa saa sa a",
-        book = "Asa Asa",
-    )
+//    QuoteShareContainer(
+//        author = "ASA",
+//        quote = "Asa saasa saa sa a",
+//        book = "Asa Asa",
+//    )
 }
