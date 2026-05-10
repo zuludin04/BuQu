7be082fa85211438710dc04cb6889797cb4476b9
zuludin <zmauludin04@gmail.com>
2026-05-10 19:59:01 +0700

    fix: prevent duplicate note when connect multiple rope

--- app/src/main/java/com/app/zuludin/buqu/data/datasources/database/dao/NoteCardDao.kt
+++ app/src/main/java/com/app/zuludin/buqu/data/datasources/database/dao/NoteCardDao.kt
@@ -18,6 +18,6 @@ interface NoteCardDao {
     @Delete
     suspend fun deleteSelectedNotes(notes: List<NoteCardEntity>)
 
-    @Query("SELECT note_card.*, CASE WHEN rope.ropeId IS NOT NULL THEN 1 ELSE 0 END AS isConnected FROM note_card LEFT JOIN rope ON note_card.noteId = rope.sourceNoteId OR note_card.noteId = rope.targetNoteId WHERE note_card.boardId = :boardId")
+    @Query("SELECT DISTINCT note_card.*, CASE WHEN rope.ropeId IS NOT NULL THEN 1 ELSE 0 END AS isConnected FROM note_card LEFT JOIN rope ON note_card.noteId = rope.sourceNoteId OR note_card.noteId = rope.targetNoteId WHERE note_card.boardId = :boardId")
     suspend fun getNotesByBoard(boardId: String): List<ConnectedNoteCardEntity>
 }
